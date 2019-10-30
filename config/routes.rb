bce8219407f0504190d02af9c4dec7add9ad3345
jmcook413 <jeremycook290@gmail.com>
2019-10-29 21:06:14 -0500

    Adds user model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   #This is just to stub out a future root path.
   #There is currently no home controller with an index action.
   root to: "home#index"
